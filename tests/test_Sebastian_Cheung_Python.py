cd766a808e278fe0bf4d6f95e94901490bf07de1
Sebastian <sebastian.cheung@yahoo.com>
2022-04-27 12:24:16 +0100

    Update incorrect user

--- tests/test_Sebastian_Cheung_Python.py
+++ tests/test_Sebastian_Cheung_Python.py
@@ -23,7 +23,7 @@ class TestGuru(TestCase):
 
     def test_ask(self):
         guru: Guru = Guru()
-        # note these values may change a little as time moves on
+        # Note these values may change a little as time moves on
         self.assertEqual('68', guru.ask('how old is Tony Blair'))
         self.assertEqual('75', guru.ask('how old is trump'))
         self.assertEqual('8908081', guru.ask('what is the population of London'))
